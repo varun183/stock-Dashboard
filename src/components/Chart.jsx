f59d7d09cc2a72d403f3622702c7f53e2e10fec3
S Varun <varunshanker1718@gmail.com>
2024-02-11 01:27:02 +0530

    create stock data chart

---
+++ src/components/Chart.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { mockHistoricalData } from "../constants/mock";
+import Card from "./Card";
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { convertUnixTimestampToDate } from "../helpers/data-helper";
+
+const Chart = () => {
+  const [filter, setFilter] = useState("1W");
+
+  const [data, setDate] = useState(mockHistoricalData);
+
+  const formatData = (data) => {
+    return data.c.map((item, index) => {
+      return {
+        value: item.toFixed(2),
+        date: convertUnixTimestampToDate(data.t[index]),
+      };
+    });
+  };
+
+  return (
+    <Card>
+      <ResponsiveContainer>
+        <AreaChart data={formatData(data)}>
+          <defs>
+            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="rgb(199 210 254)"
+                stopOpacity={0.8}
+              />
+              <stop offset="95%" stopColor="rgb(199 210 254)" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke="#312e81"
+            fillOpacity={1}
+            strokeWidth={0.5}
+            fill="url(#chartColor)"
+          />
+          <XAxis dataKey={"date"} />
+          <YAxis domain={["dataMin", "dataMax"]} />
+        </AreaChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default Chart;
