c24234ab948fcb3005752ee57090f096f2a0bd74
S Varun <varunshanker1718@gmail.com>
2024-02-11 02:22:59 +0530

    add dark mode toggle

--- src/components/ThemeIcon.jsx
+++ src/components/ThemeIcon.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MoonIcon } from "@heroicons/react/outline";
+import ThemeContext from "../context/ThemeContext";
 
 const ThemeIcon = () => {
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <button
-      className={
-        "rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg transition duration-300 hover:scale-125 "
-      }
+      className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg transition duration-300 hover:scale-125 ${
+        darkMode ? "shadow-gray-800" : null
+      }`}
+      onClick={toggleDarkMode}
     >
       <MoonIcon
-        className={
-          "h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400"
-        }
+        className={`h-8 w-8 cursor-pointer stroke-1 ${
+          darkMode
+            ? "fill-yellow-400 stroke-yellow-400"
+            : "fill-none stroke-neutral-400"
+        }`}
       />
     </button>
   );
