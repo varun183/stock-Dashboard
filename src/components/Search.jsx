4a04d4f9a87162616b618b597d8d66d45283b60e
S Varun <varunshanker1718@gmail.com>
2024-02-10 23:03:06 +0530

    create Search  button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { mockSearchResults } from "../constants/mock";
+import { XIcon, SearchIcon } from "@heroicons/react/outline";
 
 const Search = () => {
   const [input, setInput] = useState("");
@@ -23,16 +24,26 @@ const Search = () => {
         className="w-full px-4 py-2 focus:outline-none rounded-md"
         placeholder="search stock..."
         onChange={(event) => {
-          handleInputChange(event);
           setInput(event.target.value);
         }}
         onKeyDown={(event) => {
-          handleInputChange(event);
           if (event.key === "Enter") {
             updateBestMatches();
           }
         }}
       />
+
+      {input && (
+        <button onClick={clear}>
+          <XIcon className="h-4 w-4 fill-gray-500" />
+        </button>
+      )}
+      <button
+        onClick={updateBestMatches}
+        className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2 transition duration-300 hover:ring-2 ring-indigo-400"
+      >
+        <SearchIcon className="h-4 w-4 fill-gray-100" />
+      </button>
     </div>
   );
 };
