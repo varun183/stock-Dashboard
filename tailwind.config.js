f6a483b7a824d08b181d92bbca7618488c5c3eb7
S Varun <varunshanker1718@gmail.com>
2024-02-10 19:52:37 +0530

    extending theme config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 }
