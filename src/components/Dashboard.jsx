dde0ed3c5cf064d5e8eb7c4d9cf72cc822d91afe
S Varun <varunshanker1718@gmail.com>
2024-02-11 00:31:16 +0530

    Render overview and Details

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Card from "./Card";
 import { mockCompanyDetails } from "../constants/mock";
 import Header from "./Header";
+import Details from "./Details";
+import Overview from "./Overview";
 
 const Dashboard = () => {
   return (
@@ -15,10 +17,16 @@ const Dashboard = () => {
         <Card>Chart</Card>
       </div>
       <div>
-        <Card>Overview</Card>
+        <Overview
+          symbol={mockCompanyDetails.ticker}
+          price={300}
+          change={30}
+          changePercent={10.0}
+          currency={"USD"}
+        />
       </div>
       <div className="row-span-2 xl:row-span-3">
-        <Card>Details</Card>
+        <Details details={mockCompanyDetails} />
       </div>
     </div>
   );
