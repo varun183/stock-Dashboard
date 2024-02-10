b282251193497fcd96029cc42dd73ca2f7b08453
S Varun <varunshanker1718@gmail.com>
2024-02-11 00:29:07 +0530

    background and border color fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ children }) => {
   return (
     <div
-      className={`w-full h-full rounded-md relative p-8 border-2 bg-gray-300`}
+      className={`w-full h-full rounded-md relative p-8 border-2 bg-white border-neutral-200`}
     >
       {children}
     </div>
