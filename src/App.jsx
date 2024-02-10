8c363cdae8049a36ed9f71357a8237509ae74b0d
S Varun <varunshanker1718@gmail.com>
2024-02-11 04:21:53 +0530

    update stock stockContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
-import { useState } from "react";
+import StockContext from "./context/StockContext";
 import ThemeContext from "./context/ThemeContext";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("MSFT");
+
   return (
-    <>
-      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
         <Dashboard />
-      </ThemeContext.Provider>
-    </>
+      </StockContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
