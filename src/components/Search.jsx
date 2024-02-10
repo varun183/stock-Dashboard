dba942999ae96880cd19a334b195fab2b10f16cb
S Varun <varunshanker1718@gmail.com>
2024-02-11 00:29:42 +0530

    margin fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -35,7 +35,7 @@ const Search = () => {
       />
 
       {input && (
-        <button onClick={clear}>
+        <button onClick={clear} className="m-1">
           <XIcon className="h-4 w-4 fill-gray-500" />
         </button>
       )}
